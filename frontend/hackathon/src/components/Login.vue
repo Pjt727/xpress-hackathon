c2a3b96e8f1d5514bbd74867013499d9bf743ec5
Konatsuuuu <103027989+Konatsuuuu@users.noreply.github.com>
2025-02-22 16:21:11 +0900

    login

--- frontend/hackathon/src/components/Login.vue
+++ frontend/hackathon/src/components/Login.vue
@@ -10,7 +10,6 @@
         <div class="mb-4">
           <label class="block text-sm font-medium">Email</label>
           <input
-            type="email"
             v-model="email"
             class="w-full px-4 py-2 border rounded-lg bg-gray-100 text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-500"
             required
@@ -20,7 +19,7 @@
           <label class="block text-sm font-medium">Password</label>
           <input
             type="password"
-            v-modecdl="password"
+            v-model="password"
             class="w-full px-4 py-2 border rounded-lg bg-gray-100 text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-500"
             required
           />
@@ -44,21 +43,49 @@
 </template>
 
 <script>
+import axios from "axios";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
 export default {
   name: "LoginComponent",
-  data() {
-    return {
-      email: "",
-      password: "",
-    };
-  },
-  methods: {
-    handleSubmit() {
+  setup() {
+    const router = useRouter();
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = async () => {
       console.log("Logging in with:", {
-        email: this.email,
-        password: this.password,
+        email: email.value,
+        password: password.value,
       });
-    },
+
+      try {
+        const response = await axios.post("http://127.0.0.1:8000/login", {
+          email: email.value,
+          password: password.value,
+        });
+
+        if (response.data.success) {
+          console.log("Success!");
+          alert("Success!");
+          document.cookie = `user_token=${response.headers["set-cookie"]}`;
+          router.push("/history");
+        } else {
+          console.error("Failed:", response.data.message);
+          alert("Failed: " + response.data.message);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        alert("Error!");
+      }
+    };
+
+    return {
+      email,
+      password,
+      handleSubmit,
+    };
   },
 };
 </script>
